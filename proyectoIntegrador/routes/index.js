5a61237343222c17d273dc00ae97b808d3c743ec
moradiaz <moradiaz@udesa.edu.ar>
2023-03-28 17:01:06 -0300

    cambiando links

--- proyectoIntegrador/routes/index.js
+++ proyectoIntegrador/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const controladorI = require('../controllers/indexController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
---
+++ proyectoIntegrador/routes/product.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const controladorP = require('../controllers/productControllers')
+
+
+
+
+
+
+
+module.exports = router;
