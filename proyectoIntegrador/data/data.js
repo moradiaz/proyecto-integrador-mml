a94ca1aac7b95b4e7231af053376c4bf7c73060e
moradiaz <moradiaz@udesa.edu.ar>
2023-04-05 09:12:04 -0300

    data

--- proyectoIntegrador/data/data.js
+++ proyectoIntegrador/data/data.js
@@ -10,43 +10,53 @@ const data ={
     productos: [
         {nombre: "Hyaluronic Cream",
         descripcion: "Hidratante facial con ácido hialuronico, ideal para pieles mixtas a grasas, con acabado glowy",
-        fechaCarga: ""
+        fechaCarga: "15/03/23", 
+        cover: "/images/products/skin-1.jpg"
         },
         {nombre: "The Ordinary",
         descripcion: "Crema facial con vitamina C, ideal para todo tipo de piel. Actua como antioxidante y rejuvenecedora ",
-        fechaCarga: ""
+        fechaCarga: "02/04/23",
+        cover: "/images/products/skin-2.jpg"
         },
         {nombre: "Drunk Elephant Oil",
         descripcion: "Oleo facial con antioxidantes, omega 6 y 9, que ayuda a restaurar tu piel, dejando un acabado glowy ",
-        fechaCarga: ""
+        fechaCarga: "07/02/23",
+        cover:"/images/products/skin-3.jpg"
         },
         {nombre: "Drunk Elephant Cream",
         descripcion: "Crema facial, ideal para pieles secas y cansadas. Actua como barrera ante facotres estresantes ",
-        fechaCarga: ""
+        fechaCarga: "19/05/23",
+        cover:"/images/products/skin-4.jpg"
         },
         {nombre: "Unseen Sunscreen",
         descripcion: "Protector solar hidratante con SPF 40, con acabado luminoso y un instantaneo glow ",
-        fechaCarga: ""
+        fechaCarga: "06/04/23",
+        cover:"images/products/skin-5.jpg"
         },
         {nombre: "La Mer exfolator",
         descripcion: "Este exfoliante facial sensorial perfecciona la piel, elimina las toxinas presentes y la regenera ",
-        fechaCarga: ""
+        fechaCarga: "13/06/23",
+        cover:"/images/products/skin-6.jpg"
         },
         {nombre: "Dermalogica",
         descripcion: "es un suave exfoliante enzimático a base de arroz que puedes usar a diario, está perfectamente indicado para tratar las jovenes pieles grasas y acnéicas con resultados increibles ",
-        fechaCarga: ""
+        fechaCarga: "14/06/23",
+        cover:"/images/products/skin-7.jpg"
         },
         {nombre: "Drunk Elephant Body Lotion",
         descripcion: "Una loción ligera de rápida absorción con una mezcla de aceites humectantes que deja la piel con una sensación más suave, tersa y radiante.",
-        fechaCarga: ""
+        fechaCarga: "01/01/23",
+        cover:"/images/products/skin-8.jpg"
         },
         {nombre: "Glossier Serum",
         descripcion: "Serum facial que conetiene potentes extractos a base de plantas funcionan para hacer que tu piel luzca más brillante, al instante y con el tiempo ",
-        fechaCarga: ""
+        fechaCarga: "12/06/23",
+        cover:"/images/products/skin-9.jpg"
         },
         {nombre: "The Ordinary Serum",
         descripcion: "Serum facial que ayuda a mantener la correcta estructura de la piel. Retiene el agua en la epidermis, pues hidrata de forma natural",
-        fechaCarga: ""
+        fechaCarga: "08/05/23",
+        cover:"/images/products/skin-10.jpg"
         }
 
         
