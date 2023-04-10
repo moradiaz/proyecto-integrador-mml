5a5ed45f42b554165c5523ab37c862700dcb3bf5
luciana <lzeitune@udesa.edu.ar>
2023-04-10 10:06:20 -0300

    tabla productos

--- proyectoIntegrador/sql/basedeDatos.sql
+++ proyectoIntegrador/sql/basedeDatos.sql
@@ -28,3 +28,43 @@ values(default, 'danielalopez@gmail.com', 'dani.lopezz', 'danielalopez76', '/ima
 insert into usuarios(id, email, usuario, password, foto, fecha, dni)
 values(default, 'juliocruz@gmail.com', 'julio_cruz', 'juliocruz1234', '/images/users/user_image.jpeg', '1999-08-21', '40544187');
 
+create table tabla_productos (
+id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+idUsuario INT UNSIGNED NOT NULL, 
+nombreProducto TEXT NOT NULL, 
+descripcion VARCHAR(1000) NOT NULL,  
+FOREIGN KEY (idUsuario) REFERENCES usuarios(id),
+createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+deletedAt TIMESTAMP NULL
+); 
+
+insert into tabla_productos (nombreProducto, descripcion, idUsuario) 
+values ('Hyaluronic Cream', 'Hidratante facial con ácido hialuronico, ideal para pieles mixtas a grasas, con acabado glowy',2);
+
+insert into tabla_productos (nombreProducto, descripcion, idUsuario) 
+values ('The Ordinary','Crema facial con vitamina C, ideal para todo tipo de piel',3);
+
+insert into tabla_productos (nombreProducto, descripcion, idUsuario) 
+values ('Drunk Elephant Oil','Oleo facial con antioxidantes, omega 6 y 9',4);
+
+insert into tabla_productos (nombreProducto, descripcion, idUsuario) 
+values ('Drunk Elephant Cream','Crema facial, ideal para pieles secas y cansadas',5);
+
+insert into tabla_productos (nombreProducto, descripcion, idUsuario) 
+values ('Unseen Sunscreen','Protector solar hidratante con SPF 40, con acabado luminoso', 1);
+
+insert into tabla_productos (nombreProducto, descripcion, idUsuario) 
+values ('La Mer exfolator','Este exfoliante facial sensorial perfecciona la piel', 2);
+
+insert into tabla_productos (nombreProducto, descripcion, idUsuario) 
+values ('Dermalogica','es un suave exfoliante enzimático a base de arroz', 3);
+
+insert into tabla_productos (nombreProducto, descripcion, idUsuario) 
+values ('Drunk Elephant Body Lotion','Una loción ligera de rápida absorción con una mezcla de aceites humectantes',4);
+
+insert into tabla_productos (nombreProducto, descripcion, idUsuario) 
+values ('Glossier Serum','Serum de acido hialuronico que contiene vitamina B5',5);
+
+insert into tabla_productos (nombreProducto, descripcion, idUsuario) 
+values ('Lip Treatment','balsamo labial reparador que hidrata la piel', 1);
