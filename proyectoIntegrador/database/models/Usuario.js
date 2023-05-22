d8800761c2b9384a3b96050de0d11ffbf97e7aba
moradiaz <moradiaz@udesa.edu.ar>
2023-05-22 12:59:25 -0300

    modelo usuario

--- proyectoIntegrador/database/models/Usuario.js
+++ proyectoIntegrador/database/models/Usuario.js
@@ -1,2 +1,56 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = 'Usuario'
+    let cols ={
+        id:{
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        email:{
+            type: dataTypes.STRING
+        },
+        usuario:{
+            type: dataTypes.STRING
+        },
+        password:{
+            type: dataTypes.STRING
+        },
+        foto:{
+            type: dataTypes.STRING
+        },
+        fecha:{
+            type: dataTypes.DATE
+        },
+        dni:{
+            type: dataTypes.INTEGER.UNSIGNED
+        },
+        createdAt:{
+            type: dataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt:{
+            type: dataTypes.DATE,
+            allowNull: false
+        },
+        deletedAt:{
+            type: dataTypes.DATE,
+            allowNull: true
+        }
+    
+        
+    } 
+    let config = {
+        tableName: 'usuarios',
+        timestamps: false,
+        underscored: true
+    }
+    const Usuario = sequelize.define(alias, cols, config);
+    Usuario.associate = function(models){
+        Usuario.hasMany(models.Producto,{
+            as: 'products',
+            foreignKey: 'idUsuario'
+        })
+    }
 
-
+    return Usuario;
+}
