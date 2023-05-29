4b0c0295a21c68a85d99c5c3d219b962263e508a
moradiaz <moradiaz@udesa.edu.ar>
2023-05-29 10:51:47 -0300

    punto 7

--- proyectoIntegrador/controllers/indexController.js
+++ proyectoIntegrador/controllers/indexController.js
@@ -1,10 +1,30 @@
 const data= require("../data/data")
 const db = require('../database/models')
-const Product = db.Product;
+const Product = db.Producto;
 
 const indexController =  {
     principal:function(req, res, next) {
-        res.render('index', {productos:data.productos});
+        let relaciones = {
+            include:[
+                {association:'productosUsuarios'},
+                {association:'comentarios'}
+            ]
+        }
+        Product.findAll(relaciones,
+
+        {order:[
+            ['createdAt', 'DESC']
+        ]})
+        
+        .then(function(data){
+           console.log(data);
+            return res.render('index', {productos:data});
+
+        }).catch(function(error){
+            console.log(error);
+            res.send({error})
+        })
+        
       },
     index: function (req,res) {
         return res.render("register")
--- proyectoIntegrador/routes/product.js
+++ proyectoIntegrador/routes/product.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 router.get('/', productControllers.producto)
 router.get("/agregarproducto", productControllers.agregar);
-router.get('/findAll', productControllers.all)
+
 
 
 module.exports = router;
